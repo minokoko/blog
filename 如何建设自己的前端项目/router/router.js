4fe5df7d133653ef74465d77f1d9ebe99045f456
careycui <cuihaiyu@hikvision.com>
2016-12-09 18:22:04 +0800

    router

--- 如何建设自己的前端项目/router/router.js
+++ 如何建设自己的前端项目/router/router.js
@@ -4,13 +4,34 @@
 		解决方案：hash+hashChange / hTML5 history API
   */
 (function(w){
+	var _getPath = function(hash){
+		var paths = hash.split('#'),
+			path = '';
+		if(paths.length>1){
+			path = paths[1].split('?')[0];
+		}
+		return path;
+	};
+	var _getQuery = function(hash){
+		var paths = hash.split('#'),
+			query = null;
+		if(){
+			
+		}
+
+	};
 	function YsRouters(){
 		this.routers = {}; //页面路径存储{key[path]:value[object]}
 	}
 	// Router 对象初始化 url change 监听
 	YsRouters.prototype.init = function(){
-		window.addEventListener('hashChange',function(){},false);
-		window.addEventListener('load',function(){},false);
+		window.addEventListener('hashChange',function(){
+
+		},false);
+
+		window.addEventListener('load',function(){
+
+		},false);
 	};
 	// Router 注册path
 	YsRouters.prototype.map = function(path,callback){
@@ -26,10 +47,14 @@
 	};
 	//path 改变触发回调方法
 	YsRouters.prototype.urlChange = function(){
-
+		var hash = window.location.hash,
+			path = _getPath(hash),
+			query = _getQuery(hash);
 	};
 	//资源加载方法
 	YsRouters.prototype.loadAsset = function(){
 
 	};
+
+	window.ysRouters = new YsRouters();
 })(window)
